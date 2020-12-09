8d2936d7eb2e83b356818ab88ccdbabbcc8e6ca8
Scott Lucas <scott@slucas.com>
2020-12-09 14:30:38 -0800

    Initial Commit

---
+++ Shared/whf001App.swift
@@ -0,0 +1,17 @@
+//
+//  whf001App.swift
+//  Shared
+//
+//  Created by Scott Lucas on 12/9/20.
+//
+
+import SwiftUI
+
+@main
+struct whf001App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
